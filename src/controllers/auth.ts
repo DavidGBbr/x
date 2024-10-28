53da3cffb75c14ff23869649c02d966b2e7c747b
davidGBbr <davidgomesbrigido@gmail.com>
2024-10-28 08:22:11 -0300

    Rota de cadastro pt-2

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,5 +1,7 @@
 import { RequestHandler } from "express";
 import { signupSchema } from "../schemas/signup";
+import { findUserByEmail, findUserBySlug } from "../services/user";
+import slug from "slug";
 
 export const signup: RequestHandler = async (req, res) => {
   const safeData = signupSchema.safeParse(req.body);
@@ -7,5 +9,24 @@ export const signup: RequestHandler = async (req, res) => {
     res.json({ error: safeData.error.flatten().fieldErrors });
     return;
   }
+
+  const hasEmail = await findUserByEmail(safeData.data.email);
+  if (hasEmail) {
+    res.json({ error: "E-mail já existe" });
+    return;
+  }
+
+  let genSlug = true;
+  let userSlug = slug(safeData.data.name);
+  while (genSlug) {
+    const hasSlug = await findUserBySlug(userSlug);
+    if (hasSlug) {
+      let slugSuffix = Math.floor(Math.random() * 999999).toString();
+      userSlug = slug(safeData.data.name + slugSuffix);
+    } else {
+      genSlug = false;
+    }
+  }
+
   res.json({});
 };
